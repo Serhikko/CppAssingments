736a7d99003ffec424dec5a315bbc14cb37199c1
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-11-11 13:22:20 +0000

    Assingments clonned

---
+++ task1.cpp
@@ -0,0 +1,24 @@
+// ## 1. Earthquake
+
+// ### Task
+
+// The table below contains earthquake magnitude ranges on the Richter scale and their descriptors.
+//  Write a program that reads a magnitude from the user and displays the appropriate descriptor as part of a meaningful message. 
+//  For example, if the user enters 5.5 then your program should indicate that a magnitude 5.5 earthquake is considered to be a moderate earthquake.
+
+// | Magnitude             | Descriptor |
+// | ----------------------| -----------|
+// | Less than 2.0         | Mirco      |
+// | 2.0 to less than 3.0  | Very Minor |
+// | 3.0 to less than 4.0  | Minor      |
+// | 4.0 to less than 5.0  | Light      |
+// | 5.0 to less than 6.0  | Moderate   |
+// | 6.0 to less than 7.0  | Strong     |
+// | 7.0 to less than 8.0  | Major      |
+// | 8.0 to less than 10.0 | Great      |
+// | More than 10.0        | Meteoric   |
+
+// ### Key Concepts
+// Data types, variables, selection statements (if or switch)
+
+// ---
