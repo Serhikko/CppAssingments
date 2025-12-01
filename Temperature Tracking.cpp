a211de2238e1f80b6dcf470fff9aec6bccc98695
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-12-01 14:45:17 +0000

    Tasks

---
+++ Temperature Tracking.cpp
@@ -0,0 +1,10 @@
+// Task
+
+// Design a program that helps track daily temperatures for a week. The program should:
+
+//     Prompt the user to enter the temperature for each day of the week and store these into an array
+//     Calculate and display the average temperature for the entire week
+//     Identify and display how many days has a temperature above 25 degrees and output an appropriate message
+
+// Key Concepts
+// Iteration (for or while), Arrays
