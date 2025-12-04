86d8c110bee5b13124ef1ee3560de94659b94406
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-12-04 10:08:14 +0000

    Cleaning up the code
    removed majority of useless comments

--- Basic Banking.cpp
+++ Basic Banking.cpp
@@ -1,26 +1,3 @@
-// 3. Basic Banking
-// Task
-
-// Create a program that simulates a basic bank account system. The program should:
-
-//     Start with an initial balance
-//     Present the user with a menu of options
-
-// 1: Deposit
-// 2: Withdraw
-// 3: View Balance
-// 4: Exit
-
-//     Implement each operation via a separate function
-//         viewMenu: Prints the menu of options
-//         deposit(currentBalance, amount): Adds the amount to the balance
-//         withdraw(currentBalance, amount): Subtracts the amount from the balance
-//         displayBalance(currentBalance) : Prints the balance
-//     Allow the user to repeatedly display and select from the menu until they decide to exit
-
-// Key Concepts
-// Functions, Iteration
-
 
 //void menu() cout 1.deposit 2.withdraw 3.vie..//funciton that prints menu
 //void depoit(currentBalance, amount){if amount < 0}add money to balance {else cout the deposit amount must be positive}
